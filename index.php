a74e33ec2eff4773dccf512203c94704c5f2d749
carloscostantino <carloscostantino@gmail.com>
2024-03-20 11:21:57 -0300

    Comparadores lógicos

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 $nombre = "Carlos";
+echo $nombre . "<br>";
+var_dump($nombre);
+define('PULGADA', 0.25);
+
+$cm = 25;
+echo "<br>".$cm . " centímetros son: ". $cm*PULGADA . " pulgadas.";
+$x = true;
+$y=true;
+echo "<br><br>";
+
+if(!($x and $y)){
+ echo "Verdadero";
+}else echo "Falso";
+
+
 ?>
